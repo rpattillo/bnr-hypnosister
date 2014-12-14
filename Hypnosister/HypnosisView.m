eb6a9e73c89a0b56c85064c28716b66d29ec4e0c
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-14 12:25:25 -0500

    Implement custom -drawRect: on HypnosisView
    Draws a single thick light gray circle.

--- Hypnosister/HypnosisView.m
+++ Hypnosister/HypnosisView.m
@@ -10,12 +10,29 @@
 
 @implementation HypnosisView
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+   CGRect bounds = self.bounds;
+   CGPoint center;
+   center.x = bounds.origin.x + bounds.size.width / 2.0;
+   center.y = bounds.origin.y + bounds.size.height / 2.0;
+   float radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
+   
+   [[UIColor lightGrayColor] setStroke];
+   
+   UIBezierPath *path = [[UIBezierPath alloc] init];
+   [path addArcWithCenter:center
+                   radius:radius
+               startAngle:0
+                 endAngle:2 * M_PI
+                clockwise:YES];
+
+   path.lineWidth = 10;
+   
+   [path stroke];
 }
-*/
+
 
 @end

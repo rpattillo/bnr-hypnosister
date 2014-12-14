49f052c2c69ca08dd9bfc64cfd5a3a54e634144b
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-14 10:22:47 -0500

    Initial Commit

---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Ricky Pattillo on 12/14/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

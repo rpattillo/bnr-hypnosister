9885ccbbbe8618a4783290c4c91c240350d65713
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-14 12:31:18 -0500

    Default to clear background for HypnosisViews

--- Hypnosister/HypnosisView.m
+++ Hypnosister/HypnosisView.m
@@ -11,6 +11,17 @@
 @implementation HypnosisView
 
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+   self = [super initWithFrame:frame];
+   if (self ) {
+      self.backgroundColor = [UIColor clearColor];
+   }
+   
+   return self;
+}
+
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
